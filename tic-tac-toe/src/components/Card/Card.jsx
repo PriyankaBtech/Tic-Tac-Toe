a2f83cbece8764e2baacfccbe20501452a89fe95
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-09-03 23:31:26 +0530

    apply css on components

--- tic-tac-toe/src/components/Card/Card.jsx
+++ tic-tac-toe/src/components/Card/Card.jsx
@@ -1,14 +1,15 @@
 import Icon from "../Icon/Icon";
+import './Card.css'
 
-function Card(player) {
+function Card({ player }) { 
     let icon = <Icon/>
-    if(player === 'X') {
+    if (player === 'X') {
         icon = <Icon name="cross"/>
-    } else if(player === 'O') {
+    } else if (player === 'O') {
         icon = <Icon name="circle"/>
     }
     return (
-        <div className="card">
+        <div className='card'>
              {icon}
         </div>
     )
